36ffa6ba1083732de1b355ee2251b2ec769ed5bd
HarshRaj3050 <harshraj3060@gmail.com>
2026-03-15 15:12:44 +0530

    album create feature - Done, feach all music for user route - Done

--- src/controllers/music.controller.js
+++ src/controllers/music.controller.js
@@ -1,56 +1,65 @@
 const musicModel = require('../models/music.model');
 const jwt = require('jsonwebtoken');
 const { uploadFile } = require("../services/storage.service");
+const albumModel = require('../models/album.model');
 
 async function createMusic(req, res) {
-    const token = req.cookies.token;
-    console.log(token);
 
-    if (!token) {
-        return res.status(401).json({ message: "Uanuthorized" });
+    const { title } = req.body;
+    const file = req.file;
+
+    if (!file) {
+        return res.status(400).json({ message: "Music file is required" });
     }
 
+    const result = await uploadFile(file.buffer.toString('base64'));
 
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    const music = await musicModel.create({
+        uri: result.url,
+        title,
+        artist: req.user.id
+    })
 
-        if (decoded.role != "artist") {
-            return res.status(403).json({ message: "You don't have access to create an music" });
+    res.status(201).json({
+        message: "Music created successfully!",
+        music: {
+            id: music._id,
+            uri: music.uri,
+            title: music.title,
+            artist: music.artist
         }
-
-
-        const { title } = req.body;
-        const file = req.file;
-
-        if (!file) {
-            return res.status(400).json({ message: "Music file is required" });
-        }
-
-        const result = await uploadFile(file.buffer.toString('base64'));
-
-        const music = await musicModel.create({
-            uri: result.url,
-            title,
-            artist: decoded.id
-        })
-
-        res.status(201).json({
-            message: "Music created successfully!",
-            music: {
-                id: music._id,
-                uri: music.uri,
-                title: music.title,
-                artist: music.artist
-            }
-        });
-
-    }
-
-    catch (err) {
-        console.log(err);
-
-        return res.status(500).json({ message: "Internal Server Error" });
-    }
+    });
 }
 
-module.exports = { createMusic }
+async function createAlbum(req, res) {
+
+    const { title, musics } = req.body;
+
+    const album = await albumModel.create({
+        title,
+        musics: musics,
+        artist: req.user.id,
+    });
+
+
+    res.status(201).json({
+        message: "Album created successfully",
+        album: {
+            id: album._id,
+            title: album.title,
+            artist: album.artist,
+            musics: album.musics
+        }
+    })
+}
+
+async function getAllMusic(req, res) {
+    const musics = await musicModel.find().populate("artist","username");
+
+    res.status(200).json({
+        message: "Musics feached successfully",
+        musics: musics
+    })
+}
+
+module.exports = { createMusic, createAlbum, getAllMusic}
--- src/routes/music.route.js
+++ src/routes/music.route.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const musicController = require("../controllers/music.controller");
 const multer = require("multer");
+const authMiddleware = require('../middleware/auth.middleware');
 
 const storage = multer.memoryStorage()
 const upload = multer({ storage: storage })
 
 const router = express.Router();
 
-router.post("/create", upload.single("music"), musicController.createMusic);
+router.post("/create", authMiddleware.authArtist, upload.single("music"), musicController.createMusic);
+router.post("/album", authMiddleware.authArtist, musicController.createAlbum);
+
+router.get('/', musicController.getAllMusic);
 
 module.exports = router;
